2e39250c66198dd22cf1a5045388b2364f080227
rfdolan <rfdolan@wpi.edu>
2019-02-11 23:58:03 -0500

    Some more work on problem1.

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 Threads and Synchronization
 By Raymond Dolan
 
-This addignment consists of simulations for two synchronization problems.
+This assignment consists of simulations for two synchronization problems.
 
 PIRATES AND NINJAS
 
--- problem1/problem1.c
+++ problem1/problem1.c
@@ -1,11 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
+/*
+	Queue which will serve as a line
+*/
+struct node 
+{
+	// The thread at this position
+	pthread_t *info;
+	// The next thread in line
+	struct node *next;	
 
-int main(int argc, char *argv[]){
+};
+
+/*
+	Method to place a thread at the end of the line
+*/
+struct node *enqueue(struct node *head, pthread_t *ourThread)
+{
+	// If the head is null, allocate space for a new head
+	if(head == NULL)
+	{
+		struct node *newHead = malloc(sizeof(struct node));
+		newHead->info = ourThread;
+		return newHead;
+	}
+	// If not, go down the queue and find the end
+	struct node *current = head;	
+	while(current->next != NULL)
+	{
+		current = current->next;
+	}
+	// Allocate space for the new node
+	current->next = malloc(sizeof(struct node));
+	current->next->info = ourThread;
+	return head;
+}
+
+/*
+	Method to remove a thread from the front of the line
+*/
+struct node *dequeue(struct node *head)
+{
+	// If our head is the only one in line, return an empty line
+	if(head->next == NULL)
+	{
+		free(head);
+		return NULL;
+	}
+	// Else return a pointer to the new head of the line
+	struct node *current = head->next;
+	free(head);
+	return current;
+}
+
+/*
+	Struct to hold the statistics for the pirates and ninjas
+*/
+struct bill 
+{
+	int numVisits; 
+	int totalCost;
+	float *visitLengths; // Array of lengths of visits
+	float *waitTimes; // 
+};
+
+/*
+	Struct to hold the information about the day's operations
+*/
+struct dayReport
+{
+	int averageQueueLength;
+	int grossRevenue; // Amount of gold
+	int goldPerVisit; // Total gold / total visits
+	// Calculate profit by doing (grossRevenue - (5 * numTeams))
+	float *teamBusyTimes;
+	float *teamFreeTimes;
+};
+
+int main(int argc, char *argv[])
+{
 
 	// If the user gave the wrong number of arguments, inform them of usage and return.
-	if(argc != 8){
+	if(argc != 8)
+	{
 		printf("Usage: ./problem1 <numCostumingTeam> <NumPirates> <numNinjas> <avgPirateTime> <avgNinjaTime> <avgPirateArrival> <avgNinjaArrival>\n");
 		return 1;
 	}
@@ -14,29 +93,67 @@ int main(int argc, char *argv[]){
 	int numCostumingTeams = atoi(argv[1]);
 	int numPirates = atoi(argv[2]);
 	int numNinjas = atoi(argv[3]);
-	int avgPirateTime = atoi(argv[4]);
-	int avgNinjaTime = atoi(argv[5]);
-	int avgPirateArrival = atoi(argv[6]);
-	int avgNinjaArrival = atoi(argv[7]);
+	float avgPirateTime = atof(argv[4]);
+	float avgNinjaTime = atof(argv[5]);
+	float avgPirateArrival = atof(argv[6]);
+	float avgNinjaArrival = atof(argv[7]);
+
+	// Create the lines for both pirates and ninjas
+	struct queue pirateQueue;
+	struct queue ninjaQueue;
 
 	// If the user's input violates any of the min or max numbers, let them know and stop execution.
-	if((4 < numCostumingTeams) || (numCostumingTeams < 2)){
+	if((4 < numCostumingTeams) || (numCostumingTeams < 2))
+	{
 		printf("Please input a costuming team number ranging from 2 to 4.\n");
 		return 1;
 	}
-	if( (10 > numPirates) || (numPirates > 50)){
+	if( (10 > numPirates) || (numPirates > 50))
+	{
 		printf("Please input a number of pirates ranging from 10 to 50.\n");
 		return 1;
 	}
 	
-	if( (10 > numNinjas) || (numNinjas > 50)){
+	if( (10 > numNinjas) || (numNinjas > 50))
+	{
 		printf("Please input a number of ninjas ranging from 10 to 50.\n");
 		return 1;
 	}
 
+	if( (0 > avgPirateTime) )
+	{
+		printf("Please input a positive avgPirateTime.\n");
+		return 1;
+	}
+	if( (0 > avgNinjaTime) )
+	{
+		printf("Please input a positive avgNinjaTime.\n");
+		return 1;
+	}
+	if( (0 > avgPirateArrival) )
+	{
+		printf("Please input a positive avgPirateArrival.\n");
+		return 1;
+	}
+	if( (0 > avgNinjaArrival) )
+	{
+		printf("Please input a positive avgNinjaArrival.\n");
+		return 1;
+	}
+
+	for(int i=0; i<numPirates; i++)
+	{
+		// Create a random pirate thread
+		// Put it into the line
+	}
+
+	for(int i=0; i<numNinjas; i++)
+	{
+		// Create a random ninja thread
+		// Put it into the line
+	}
+
 	//TODO
-	// Create and randomize each pirate and each ninja.
-	// Create a waiting line for pirates and for ninjas.
 	// run the simulation
 	// Calculate expenses.
 		
